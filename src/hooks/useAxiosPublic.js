73baa60c97d022ef33e7002193f8a40faafdc877
yasin al hasan <ya705668@gmail.com>
2024-06-05 17:10:56 +0600

    creat a new castom hook and send user info in server

---
+++ src/hooks/useAxiosPublic.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+import axios from "axios";
+
+const axiosPublic = axios.create(
+    {
+        baseURL: 'http://localhost:5000'
+    }
+);
+
+const useAxiosPublic = () => {
+    return axiosPublic;
+};
+
+export default useAxiosPublic;
--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -4,9 +4,10 @@ import { useForm } from 'react-hook-form';
 import { AuthContext } from '../../providers/AuthProvider';
 import { Link, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
 
 const SignUp = () => {
-
+  const axiosPublic = useAxiosPublic();
   // =================== react form components 
   // ==================== useForm hook 
     const {
@@ -31,8 +32,16 @@ const SignUp = () => {
         console.log(loggedUser)
         updateUserProfile(data.name, data.photoURL)
         .then(() => {
-          console.log('user profile info updated')
-          reset();
+          // create user entry in the database
+          const userInfo = {
+            name: data.name,
+            email: data.email
+          }
+          axiosPublic.post('/users', userInfo)
+          .then(res => {
+            if(res.data.insertedId){
+              console.log('user added to the database')
+              reset();
           Swal.fire({
             position: "top-end",
             icon: "success",
@@ -41,6 +50,9 @@ const SignUp = () => {
             timer: 1500
           });
           navigate('/');
+            }
+          })
+          
         })
         .catch(error => console.log(error))
       })
