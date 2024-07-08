34b77bb4c8ab087a2e9c2b13b41b135ea0d5acaa
yasin al hasan <ya705668@gmail.com>
2024-07-08 23:30:40 +0600

    some design update

--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+// import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel  } from 'react-responsive-carousel';
+
 
 import img1 from '../../../assets/img/home/01.jpg'
 import img2 from '../../../assets/img/home/02.jpg'
@@ -11,18 +12,20 @@ import img4 from '../../../assets/img/home/04.jpg'
 const Banner = () => {
     return (
         <div>
-        <Carousel>
+        <Carousel  >
         <div>
             <img src={img1} />
-            <p className="legend">Legend 1</p>
         </div>
         <div>
             <img src={img2} />
-            <p className="legend">Legend 2</p>
         </div>
         <div>
             <img src={img3} />
-            <p className="legend">Legend 3</p>
+            
+        </div>
+        <div>
+            <img src={img4} />
+            
         </div>
     </Carousel>
         </div>
--- src/pages/shared/navbar/Navbar.jsx
+++ src/pages/shared/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { AuthContext } from '../../../providers/AuthProvider';
 import { IoCartSharp } from "react-icons/io5";
 import useCart from '../../../hooks/useCart';
 import useAdmin from '../../../hooks/useAdmin';
+import profile from '../../../../public/profile.png'
 
 const Navbar = () => {
 
@@ -17,12 +18,15 @@ const Navbar = () => {
     .then(() => {})
     .catch(console.error(error));
   }
+  // const displayName = user?.displayName;
+  // const userProfile = displayName.split('');
+  // const profile = userProfile[0]
+  // console.log(displayName, userProfile, profile)
 
 
     const navOption = <>
     <li> <Link to="/" >Home</Link> </li>
-    <li> <Link to="/menu" >Our Menu</Link> </li>
-    <li> <Link to="/order/salad" >Food Order </Link> </li>
+    <li> <Link to="/menu" >Contact Us</Link> </li>
     {
       // user ? 'true' : 'false' <== ternary operator
       // user ? condition ? 'double true' : 'one true' : 'false' <== nested operator
@@ -33,11 +37,14 @@ const Navbar = () => {
     {
       user && !isAdmin && <li> <Link to="/dashboard/userHome" >Dashboard </Link> </li>
     }
-    <li> <Link to="/dashboard/cart" > 
-    <button className="btn">
+    <li> <Link to="/menu" >Our Menu</Link> </li>
+    <li> <Link to="/order/salad" >Food Order </Link> </li>
+    <li> <Link to="/dashboard/cart" className=" " > 
+    
+    Our Shop
     <IoCartSharp></IoCartSharp> 
   <div className="badge badge-secondary">+{cart.length}</div>
-</button>
+
     </Link> </li>
    <li>
    {
@@ -54,6 +61,9 @@ const Navbar = () => {
   }
    </li>
     </>
+
+    
+    
     return (
         <div>
         <div className="navbar fixed z-10 bg-opacity-30 text-white bg-black">
@@ -62,20 +72,25 @@ const Navbar = () => {
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-[#111111db] rounded-box w-52">
         {navOption}
       </ul>
           </div>
-          <Link to="/" className="btn btn-ghost text-xl">Bangla Restaura</Link>
+          <Link to="/" className="btn btn-ghost text-xl uppercase">Bangla Restaura</Link>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul className="menu menu-horizontal px-1 ">
             {navOption}
           </ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn">{user?.email}</a>
+        {
+        user ? <div className="navbar-end flex">
+        <img src={user?.photoURL} className="avatar ring-primary ring-offset- w-12 mr-2 rounded-full" alt="" />
+          <a className="btn btn-outline uppercase ">{user?.displayName}</a>
         </div>
+        : 
+        <div></div>
+        }
       </div>
         </div>
     );
